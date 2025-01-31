45bb81491143d28413c40ad357c3b33e63e38c93
Pablo Bozzolo <boctulus@gmail.com>
2025-01-31 13:55:50 +0100

    Cache

--- app/Http/Controllers/TestFallback.php
+++ app/Http/Controllers/TestFallback.php
@@ -10,10 +10,12 @@ class TestFallback extends Controller
     function test_apiclientfallback_package(){
         $client = new ApiClientFallbackAlias();
 
+        $client->cache(3600);
+
         $client
         ->setHeaders([
             "Content-type" => "application/json"
-        ])
+        ])        
         ->get("https://jsonplaceholder.typicode.com/posts/1");
 
         $res = $client->getResponse();
